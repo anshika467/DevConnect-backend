b2d2a52f2ea749f8d28a47cc2aa9ce177d6070f9
Anshika Saxena <developer.anshika467@gmail.com>
2025-09-04 17:37:30 +0530

    Secured DB Credentials

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/database");
+
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://namastenodejs:dSTSsfetqZP0xg4a@namastenode.bwcu78w.mongodb.net/devTinder"
+    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@namastenode.bwcu78w.mongodb.net/devTinder`
   );
 };
 
